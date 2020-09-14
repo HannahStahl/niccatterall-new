9ac64dd54a90b339ebe840ae5e4953f88bdd741a
Hannah Stahl <hls62@cornell.edu>
2020-09-14 18:01:06 -0500

    Added past clients to Clients page

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -2,25 +2,37 @@ import React from 'react';
 import config from '../config';
 import content from '../content.json';
 
-const getClients = () => content.clients.players.sort((a, b) => {
+const clients = content.clients.players.sort((a, b) => {
   if (a.lastName < b.lastName) return -1;
   if (b.lastName < a.lastName) return 1;
   return 0;
 });
 
+const currentClients = clients.filter((client) => client.active);
+const pastClients = clients.filter((client) => !client.active);
+
+const Client = ({ client }) => (
+  <a
+    href={client.itemLink.startsWith('http') ? client.itemLink : `https://${client.itemLink}`}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="item client-preview-container"
+  >
+    <img src={`${config.photosCloudfrontURL}/clients/${client.itemPhoto}`} alt={client.itemName} />
+    <h4>{`${client.firstName} ${client.lastName}`}</h4>
+  </a>
+);
+
 export default () => (
-  <div className="items">
-    {getClients().map((client) => (
-      <a
-        key={client.itemLink}
-        href={client.itemLink.startsWith('http') ? client.itemLink : `https://${client.itemLink}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="item client-preview-container"
-      >
-        <img src={`${config.photosCloudfrontURL}/clients/${client.itemPhoto}`} alt={client.itemName} />
-        <h3>{`${client.firstName} ${client.lastName}`}</h3>
-      </a>
-    ))}
-  </div>
+  <>
+    <h3 className="clients-header">Players currently working with Nic:</h3>
+    <div className="items">
+      {currentClients.map((client) => <Client key={client.itemLink} client={client} />)}
+    </div>
+    <hr />
+    <h3 className="clients-header">Players who have previously worked with Nic:</h3>
+    <div className="items">
+      {pastClients.map((client) => <Client key={client.itemLink} client={client} />)}
+    </div>
+  </>
 );
