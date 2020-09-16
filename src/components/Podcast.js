fb1218a1969a38efbd808f9843e94307e8d2f042
Hannah Stahl <hls62@cornell.edu>
2020-09-16 09:00:19 -0500

    Added podcast episodes to Podcast page

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import config from '../config';
 
-export default () => (
-  <>
-    <div className="items" />
-  </>
-);
+export default () => {
+  const [episodeURLs, setEpisodeURLs] = useState([]);
+
+  useEffect(() => {
+    fetch(config.podcastURL, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    }).then((response) => response.json()).then((episodes) => {
+      setEpisodeURLs(episodes.map((episode) => {
+        const url = episode.audio_url.split('.mp3')[0];
+        return `${url}?client_source=small_player&amp;iframe=true&amp;referrer=${url}.js?player=small`;
+      }));
+    });
+  }, []);
+
+  return (
+    <div>
+      {episodeURLs.map((episodeURL) => (
+        <iframe
+          key={episodeURL}
+          src={episodeURL}
+          className="podcast-episode"
+          frameBorder="0"
+          scrolling="no"
+          title="Podcast"
+        />
+      ))}
+    </div>
+  );
+};
--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 const dev = {
   apiURL: 'https://dlnum6f7kj.execute-api.us-east-1.amazonaws.com/dev',
   cloudfrontBaseURL: 'https://d1ljva6zkf6zjh.cloudfront.net',
-  emailURL: 'https://c0mrk8va37.execute-api.us-east-1.amazonaws.com/dev/email/send',
-  emailAddress: 'hannahstahl14@gmail.com',
+  podcastURL: "https://mtmhzl2lje.execute-api.us-east-1.amazonaws.com/dev/episodes/get",
   userID: 'us-east-1:0c9864e8-7db2-45fc-9c5a-ed2c11e2d9cf',
   blogConfigId: '3',
   programsConfigId: '1',
@@ -11,8 +10,7 @@ const dev = {
 const prod = {
   apiURL: 'https://lbe32id9hg.execute-api.us-east-1.amazonaws.com/prod',
   cloudfrontBaseURL: 'https://d1esxin5o90ebg.cloudfront.net',
-  emailURL: 'https://aiikn63n03.execute-api.us-east-1.amazonaws.com/prod/email/send',
-  emailAddress: 'catterallnic@gmail.com',
+  podcastURL: "https://btsrfouie1.execute-api.us-east-1.amazonaws.com/prod/episodes/get",
   userID: 'us-east-1:eb9f731c-e691-4bc2-a74c-ba3e0cfd4f6b',
   blogConfigId: '4',
   programsConfigId: '5',
@@ -26,7 +24,6 @@ const config = process.env.REACT_APP_STAGE === 'prod'
 export default {
   // Add common config values here
   cloudfrontURL: `${config.cloudfrontBaseURL}/${config.userID}`,
-  publicCloudfrontURL: 'https://d17jmxltsx3ffm.cloudfront.net',
   photosCloudfrontURL: 'https://d2vff3mk7ylu21.cloudfront.net',
   businessName: 'Nic Catterall',
   ...config,
