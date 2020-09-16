9a08e1267e40a8553cf92b5dfba422a27be4285b
Hannah Stahl <hls62@cornell.edu>
2020-09-16 14:20:20 -0500

    Added affiliate links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import config from '../config';
+import content from '../content.json';
 
 const Footer = () => (
   <div className="footer">
+    <div className="affiliates">
+      {content.affiliates.map(({ image, alt, href }) => (
+        <a key={alt} href={href} target="_blank" rel="noopener noreferrer">
+          <img src={`${config.photosCloudfrontURL}/affiliates/${image}`} alt={alt} />
+        </a>
+      ))}
+    </div>
     <p>
       {`© ${config.businessName}, ${(new Date()).getFullYear()}. All Rights Reserved.`}
     </p>
